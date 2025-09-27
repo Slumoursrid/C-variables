18e2b986e39f3298c1c4b047711fda1c6074f8e5
NamMel <namikmellalhomecom@gmail.com>
2025-09-27 19:09:06 +0200

    the_average.cpp
    A small program that calcul the average of 5 integers input by user.

--- the_average.cpp
+++ the_average.cpp
@@ -1,1 +1,18 @@
-
+#include <iostream>
+using namespace std;
+// User enters 5 integers and this program calculates their average
+int main(){
+    cout << "Enter your integers: \n";                                                            
+    
+  // Declaration of 5 variables
+    int integer1, integer2, integer3, integer4, integer5;                                
+    
+  // User inputs their values that are assigned to the variables respectively
+    cin >> integer1 >> integer2 >> integer3 >> integer4 >> integer5;
+   
+  // A simple calculation to find the average of the 5 variables
+    int average_calculation = (integer1 + integer2 + integer3 + integer4 + integer5) / 5;
+    
+  // The program output
+    cout << "The average is: " << average_calculation;
+}
