487a9dda9944071b57cf47690f120b72e66edd50
NamMel <namikmellalhomecom@gmail.com>
2025-10-20 21:37:55 +0200

    Add palindrome checking function
    Implement isItAPalindrome function to check for palindromes.

---
+++ IsItAPalindrome.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+//is it  a palindrome function
+bool isItAPalindrome(string userEntry){
+//number of caractere in a word
+size_t totalLetter = userEntry.size();
+
+// beginning point
+int firstLetter = 0;
+int lastLetter = totalLetter - 1;
+
+
+for(letter <= totalLetter; letter ++;){
+        int firstLetter = userEntry[0];
+        int lastLetter = totalLetter - 1 ;
+}
+
+
+
+
+
+return 0;
+}
+
+
+int main(){
+    string entry_user{};
+
+    std::cout << "entre ton mot";
+    std::cin >> entry_user;
+    //size_t totalLetter = entry_user.length();
+    //std::cout << totalLetter;
+
+
+
+
+}
+
